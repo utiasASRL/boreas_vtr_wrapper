d0d92d5d7af844d8759ce88d129cdf94cac0f6b6
Daniil <lisus.daniil@gmail.com>
2026-03-17 11:52:46 -0400

    Updated build command to use more threads

--- scripts/build_boreas_vtr_wrapper.sh
+++ scripts/build_boreas_vtr_wrapper.sh
@@ -4,7 +4,7 @@ export VTRSRC=$ROOTDIR/external/vtr3
 source /opt/ros/humble/setup.bash
 source ${VTRSRC}/main/install/setup.bash # source the vtr3 environment
 cd $ROOTDIR/src # go to the root directory
-MAKEFLAGS="-j$(($(nproc --all) / 20 + 1))" colcon build --parallel-workers 1 --packages-select vtr_testing_common vtr_testing_lidar vtr_testing_radar vtr_testing_radar_lidar vtr_testing_aeva --packages-ignore $(colcon list | grep -v -E 'vtr_testing_common|vtr_testing_aeva|vtr_testing_lidar|vtr_testing_radar|vtr_testing_radar_lidar' | awk '{print $1}' | tr '\n' ' ') --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+MAKEFLAGS="-j$(($(nproc --all) / 4 + 1))" colcon build --parallel-workers 1 --packages-select vtr_testing_common vtr_testing_lidar vtr_testing_radar vtr_testing_radar_lidar vtr_testing_aeva --packages-ignore $(colcon list | grep -v -E 'vtr_testing_common|vtr_testing_aeva|vtr_testing_lidar|vtr_testing_radar|vtr_testing_radar_lidar' | awk '{print $1}' | tr '\n' ' ') --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 
 cd $ROOTDIR
